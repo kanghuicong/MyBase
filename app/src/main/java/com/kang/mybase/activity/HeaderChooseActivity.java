1920b54101891a780546fc21ed3fff2cd2b6d0a2
kanghuicong <515849594@qq.com>
2017-12-18 16:55:17 +0800

    头像选择完成 --12.18

--- app/src/main/java/com/kang/mybase/activity/HeaderChooseActivity.java
+++ app/src/main/java/com/kang/mybase/activity/HeaderChooseActivity.java
@@ -1,14 +1,19 @@
 package com.kang.mybase.activity;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import com.kang.headerpicker.CircleImageView;
 import com.kang.headerpicker.PicUtils;
+import com.kang.headerpicker.SDPathUtils;
 import com.kang.mybase.R;
 import com.kang.mybase.base.BaseActivity;
 import com.kang.mybase.custom.MyBottomDialog;
 import com.kang.mybase.pro.IDialogBottom;
 import com.kang.mybase.util.inject.InjectActivityView;
+
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import butterknife.InjectView;
@@ -16,6 +21,7 @@ import butterknife.OnClick;
 
 import static com.kang.headerpicker.PicUtils.CHOOSE_HEADER;
 import static com.kang.headerpicker.PicUtils.PREVIEW_HEADER;
+import static com.kang.headerpicker.PicUtils.TAKE_PICTURE_HEADER;
 import static com.kang.utilssdk.ToastUtils.showShort;
 
 /**
@@ -27,6 +33,7 @@ public class HeaderChooseActivity extends BaseActivity implements IDialogBottom 
 
 
     List<String> list = new ArrayList<>();
+    List<String> pathList = new ArrayList<>();
     @InjectView(R.id.iv_header)
     CircleImageView ivHeader;
 
@@ -49,7 +56,7 @@ public class HeaderChooseActivity extends BaseActivity implements IDialogBottom 
     public void onViewClicked() {
         new MyBottomDialog(this, list, this);
     }
-
+    private Uri imageFileUri;
     @Override
     public void iDialog(int position) {
         switch (position) {
@@ -59,6 +66,12 @@ public class HeaderChooseActivity extends BaseActivity implements IDialogBottom 
                 startActivityForResult(intent, CHOOSE_HEADER);
                 break;
             case 1:
+                String image_path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + System.currentTimeMillis() + ".jpg";
+                File file = new File(image_path);
+                imageFileUri = Uri.fromFile(file);
+                Intent it = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                it.putExtra(MediaStore.EXTRA_OUTPUT, imageFileUri);
+                startActivityForResult(it, PicUtils.TAKE_PICTURE_HEADER);
                 break;
         }
     }
@@ -66,18 +79,29 @@ public class HeaderChooseActivity extends BaseActivity implements IDialogBottom 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CHOOSE_HEADER && data != null) {
+        if (requestCode == PicUtils.CHOOSE_HEADER && data != null) {
             //调系统图库，选择返回立即进入裁剪界面
             PicUtils.startPhotoZoom(this, data.getData());
-        } else if (requestCode == PREVIEW_HEADER) {
+        }else if (requestCode == PicUtils.TAKE_PICTURE_HEADER){
+            PicUtils.startPhotoZoom(this, imageFileUri);
+        } else if (requestCode == PicUtils.PREVIEW_HEADER) {
             //裁剪图片并返回地址。按照实际开发情况，在头像上传完了后应该调用
             if (data != null) {
                 String path = PicUtils.setPicToView(this, ivHeader, data);
-                showShort(path);
-//                Glide.with(this).load(path).error(R.drawable.defalut_header).into(ivHeader);
-
+                pathList.add(path);
             }
         }
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //通常来说，选择完头像后会做上传操作，第二次进来后看到的头像应该是从网络读取的图片
+        //所以activity退出后本地的裁剪图片应该清除
+        if (!pathList.isEmpty()) {
+            for (int i=0;i<pathList.size();i++) {
+                SDPathUtils.delFile(pathList.get(i));//删除裁剪地址的图片
+            }
+        }
+    }
 }
